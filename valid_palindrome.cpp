ddb47fafe789c65eecf7f83d9c56f5621b340fa2
Yash-Jumde <133998178+Yash-Jumde@users.noreply.github.com>
2023-11-21 06:24:20 +0530

    Create valid_palindrome.cpp
    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
    
    Given a string s, return true if it is a palindrome, or false otherwise.

---
+++ valid_palindrome.cpp
@@ -0,0 +1,34 @@
+// Concept : Two Pointers
+
+// Question : Valid Palindrome
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int i=0;i<s.length();)
+        {
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))
+                i++;
+            else if(s[i]>='A' && s[i]<='Z')
+            {
+                s[i] += 32;
+                i++;
+            }
+            else{
+                s.erase(s.begin()+i);
+            }
+        }
+
+        if(s.empty())
+            return true;
+        
+        int right = s.length()-1;
+        for(int left = 0;left <= right; left++, right--)
+        {
+            if(s[left]!=s[right])
+                return false;
+        }
+        return true;
+
+    }
+};
