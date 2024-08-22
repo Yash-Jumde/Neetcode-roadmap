281efabd9f9fcab1a8e591d8e71037874ab57a0f
Yash-Jumde <133998178+Yash-Jumde@users.noreply.github.com>
2024-08-22 14:54:17 +0530

    Update Valid_Anagram.cpp

--- Valid_Anagram.cpp
+++ Valid_Anagram.cpp
@@ -20,3 +20,23 @@ public:
         return true;
     }
 };
+
+// or
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int s1 = s.size();
+        int s2 = t.size();
+        if(s1!=s2) return false;
+
+        unordered_map<char, int> m;
+        for(int i=0;i<s1;i++) m[s[i]]++;
+        for(int j=0;j<s2;j++) {
+            m[t[j]]--;
+            if(m[t[j]]==0) m.erase(t[j]);
+        }
+
+        if(m.empty()) return true;
+        return false;
+    }
+};
